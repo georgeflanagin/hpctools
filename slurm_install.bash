3732025fddbd0477c4fe5a82d66664099d41256b
George Flanagin <gflanagi@richmond.edu>
2021-05-13 13:41:37 -0400

    A good bit of progress.

--- slurm_install.bash
+++ slurm_install.bash
@@ -1,6 +1,12 @@
 ###
 # THIS IS VERSION 2.0 of this script. It is based on the version mentioned
 # in ni-sp.com's Copyright statement below.
+# 
+# Changes: 
+#  1. New comments are marked with # >>>>>>>>>>> rather than the row of #.
+#  2. Additionally, this is straight-up bash rather than ambiguously shell.
+#  3. $result is used as a placeholder for string results from operations
+#     that check for things.
 ###
 export __author__="George Flanagin"
 export __version_date__=2021
@@ -29,108 +35,215 @@ export __email__="gflanagin@richmond.edu"
 # https://slurm.schedmd.com/quickstart_admin.html
 # https://wiki.fysik.dtu.dk/niflheim/Slurm_installation
 # https://slurm.schedmd.com/faq.html
-
 # In case of version 7 "Compute Node" was the base for the installation
 # In case of version 8 "Server" was the base for the installation
-
 # SLURM accounting support
-sudo dnf -y install mariadb-server mariadb-devel
+##################################################################################
+
+# >>>>>>>>>>
+# Make sure this is bash, and stop if we are not.
+# >>>>>>>>>>
+this_shell=basename $(readlink /proc/$$/exe)
+if [ $this_shell != "bash" ]; then
+    echo "This script should be run after you are in the bash shell."
+    echo "Try again after typing 'bash'."
+    return
+fi
+
+# >>>>>>>>>>
+# Make sure this user can sudo
+# >>>>>>>>>>
+g=$(groups)
+if [ "$g" == *"wheel"* || "$g" == *"sudo"* ]; then
+    echo "Good thing you can sudo."
+else
+    echo "Only sudoers can run this script."
+    return
+fi
+
+# >>>>>>>>>>
+# NOTE: update the default version if required.
+# >>>>>>>>>>
+if [ "$VER" == "" ]; then
+    export VER=20.11.3
+fi
+
+# >>>>>>>>>>
+# Collect info about this machine.
+# >>>>>>>>>>
+export HOST=`hostname`
+ip=$(hostname -I)
+export HOST_IP=$(for x in $ip; do echo $x; break; done)
+
+# >>>>>>>>>>>
+# set the variables for compute and head nodes.
+# >>>>>>>>>>>
+case "$1" in 
+    "-?"|"?"|"help")
+    cat - <<EOF
+Usage:
+    This script will do its best to install and upgrade the 
+    slurm/munge environment on this node of the cluster. The
+    default is to configure for a compute node because there
+    usually many of them. To install for the head node, add
+    the word "head" to the command line.
+EOF
+    return
+    ;;
+
+    ""|"compute")
+    export compute_node=1
+    export head_node=0
+    ;;
+
+    "head")
+    export computer_node=0
+    export head_node=1
+    ;;
+
+    *)
+    echo "Syntax: $0 {help | compute | head}"
+    return
+    ;;
+    
+esac
+
+# >>>>>>>>>>
+# Identify the right installer. We are going to call it
+# dnf no matter what.
+# >>>>>>>>>>
+dnf=$(which dnf 2>/dev/null)
+if [ -z $dnf ]; then
+    dnf=$(which yum)
+fi
+
+# >>>>>>>>>>
+# Identify the correct rpm package tool.
+# >>>>>>>>>>
+export rpmbuilder=$(which rpm-build 2>/dev/null)
+if [ ! $? ];
+    export rpmbuilder=$(which rpmbuild 2>/dev/null)
+fi
+
+# >>>>>>>>>>>
+# Avoid typos by setting this env variable to ensure
+# we are installing everything the same way. Might as
+# well get the latest and greatest.
+# >>>>>>>>>>>
+alias installit="sudo $dnf -y upgrade" 
+
+# >>>>>>>>>>
+# Install the up to scratch database.
+# >>>>>>>>>>
+installit mariadb-server mariadb-devel
 
 # For all the nodes, before you install Slurm or Munge:
-
-# check for RH/CentOS Version
-OSVERSION="7"
-[ "`hostnamectl | grep Kernel | grep el8`" != "" ] && OSVERSION="8"
-
-# sudo su -
-export MUNGEUSER=966
-sudo groupadd -g $MUNGEUSER munge
-sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+# >>>>>>>>>>
+# Setup some defaults, and check for previously existing values
+# for the slurm and munge users.
+# >>>>>>>>>>
+echo "Checking for munge user"
+export MUNGEUSER=996
+result=$(id -u munge 2>/dev/null)
+if [ -z $result ]; then
+    sudo groupadd -g $MUNGEUSER munge
+    sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+else
+    export MUNGEUSER=$result
+fi
+echo "munge user is $MUNGEUSER"
+    
+echo "Checking for slurm user"
 export SLURMUSER=967
-sudo groupadd -g $SLURMUSER slurm
-sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
-# exit
-
-# For CentOS 7: need to get the latest EPEL repository.
-sudo dnf -y install epel-release
-if [ "$OSVERSION" == "7" ] ; then
-    sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y 
+result=$(id -u slurm 2>/dev/null)
+if [ -z $result ]; then
+    sudo groupadd -g $SLURMUSER slurm
+    sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+else
+    export SLURMUSER=$result
 fi
-if [ "$OSVERSION" == "8" ] ; then
-    sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y 
-fi
+echo "slurm user is $SLURMUSER"
 
+# >>>>>>>>>>>>>>
 # install munge
-if [ "$OSVERSION" == "7" ] ; then
-    sudo yum install munge munge-libs munge-devel -y
-fi
-if [ "$OSVERSION" == "8" ] ; then
-    sudo yum install munge munge-libs  -y
-    dnf --enablerepo=PowerTools install munge-devel -y
-fi
-sudo yum install rng-tools -y
+# >>>>>>>>>>>>>>
+installit munge munge-libs munge-devel
+installit rng-tools
 sudo rngd -r /dev/urandom
 
-sudo /usr/sbin/create-munge-key -r -f
+# >>>>>>>>>>>>>>
+# do not overwrite the mungekey if this step has been previously done.
+# >>>>>>>>>>>>>>
+export mungekey="/etc/munge/munge.key"
+key_exists=$(sudo ls $mungekey 2>/dev/null)
+if [ -z $key_exists ]; then
+    sudo /usr/sbin/create-munge-key -r -f
+    sudo chown munge: /etc/munge/munge.key
+    sudo chmod 400 /etc/munge/munge.key
+fi
 
-sudo sh -c  "dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key"
-sudo chown munge: /etc/munge/munge.key
-sudo chmod 400 /etc/munge/munge.key
-
+# >>>>>>>>>>>>>>>
+# Hang onto the hash of the key, and then start munge.
+# >>>>>>>>>>>>>>>
+key_hash=$(sudo sha1sum /etc/munge/munge.key)
+export key_hash=$(for x in $key_hash; do echo $x; break; done)
 sudo systemctl enable munge
 sudo systemctl start munge
 
-# build and install SLURM 
-sudo yum install python3 gcc openssl openssl-devel pam-devel numactl numactl-devel hwloc lua readline-devel ncurses-devel man2html libibmad libibumad rpm-build  perl-ExtUtils-MakeMaker.noarch -y
-if [ "$OSVERSION" == "7" ] ; then
-    sudo yum install rrdtool-devel lua-devel hwloc-devel -y
-fi
-if [ "$OSVERSION" == "8" ] ; then
-    yum install rpm-build make -y
-    dnf --enablerepo=PowerTools install rrdtool-devel lua-devel hwloc-devel -y
-    # dnf group install "Development Tools"
-fi
 
-mkdir slurm-tmp
+# build and install SLURM 
+installit python3 gcc openssl openssl-devel pam-devel \
+    numactl numactl-devel hwloc lua readline-devel \
+    ncurses-devel man2html libibmad libibumad rpm-build \
+    perl-ExtUtils-MakeMaker.noarch
+
+installit rrdtool-devel lua-devel hwloc-devel
+
+mkdir -p slurm-tmp
 cd slurm-tmp
-if [ "$VER" == "" ]; then
-    export VER=20.02.6    # latest 20.02.XX version
-    export VER=20.11.3
-fi
-# https://download.schedmd.com/slurm/slurm-20.02.3.tar.bz2
 wget https://download.schedmd.com/slurm/slurm-$VER.tar.bz2
 
-rpmbuild -ta slurm-$VER.tar.bz2    # and wait a few minutes until SLURM has been compiled
-# if [ "$OSVERSION" == "7" ] ; then
-# fi
-# if [ "$OSVERSION" == "8" ] ; then
-#     rpm-build -ta slurm-$VER.tar.bz2    # and wait a few minutes until SLURM has been compiled 
-# fi
+$rpmbuild -ta slurm-$VER.tar.bz2    # and wait a few minutes until SLURM has been compiled
 
-rm slurm-$VER.tar.bz2
 cd ..
-rmdir slurm-tmp 
+rmdir -fr slurm-tmp 
 
-# get perl-Switch
-# sudo yum install cpan -y 
-
+mkdir -p ~/rpmbuild/RPMS/x86_64
 cd ~/rpmbuild/RPMS/x86_64/
 
-# skipping slurm-openlava and slurm-torque because of missing perl-Switch 
-sudo yum --nogpgcheck localinstall slurm-[0-9]*.el*.x86_64.rpm slurm-contribs-*.el*.x86_64.rpm slurm-devel-*.el*.x86_64.rpm \
-slurm-example-configs-*.el*.x86_64.rpm slurm-libpmi-*.el*.x86_64.rpm  \
-slurm-pam_slurm-*.el*.x86_64.rpm slurm-perlapi-*.el*.x86_64.rpm slurm-slurmctld-*.el*.x86_64.rpm \
-slurm-slurmd-*.el*.x86_64.rpm slurm-slurmdbd-*.el*.x86_64.rpm -y
+# >>>>>>>>>>>>>>
+# Now install the rpms we just built. Our packages are unsigned
+# because we just built them ourselves, so skip that bit of checking
+# >>>>>>>>>>>>>>
+installit --nogpgcheck localinstall \
+    slurm-[0-9]*.x86_64.rpm \
+    slurm-contribs-*.x86_64.rpm \
+    slurm-devel-*.x86_64.rpm \
+    slurm-example-configs-*.x86_64.rpm \
+    slurm-libpmi-*.x86_64.rpm  \
+    slurm-pam_slurm-*.x86_64.rpm \
+    slurm-perlapi-*.x86_64.rpm \
+    slurm-slurmctld-*.x86_64.rpm \
+    slurm-slurmd-*.x86_64.rpm \
+    slurm-slurmdbd-*.x86_64.rpm
 
+# >>>>>>>>>>>>
+# By this time, we should have slurmd installed, so let's use
+# it to find out the config.
+# >>>>>>>>>>>>
+slurm_cfg=$(slurmd -C | head -1)
+export slurm_cfg="$slurm_cfg NodeAddr=$HOST_IP"
+
+# >>>>>>>>>>>
 # create the SLURM default configuration with
-# compute nodes called "NodeName=linux[1-32]"
+# compute nodes called "NodeName=$HOST"
 # in a cluster called "cluster"
 # and a partition name called "test"
 # Feel free to adapt to your needs
-HOST=`hostname`
-
+# >>>>>>>>>>>>
 sudo cat > /etc/slurm/slurm.conf << EOF
-
+# This file was generated on `date` by $0
+# 
 # slurm.conf file generated by configurator easy.html.
 # Put this file on all nodes of your cluster.
 # See the slurm.conf man page for more information.
@@ -179,19 +292,14 @@ JobAcctGatherType=jobacct_gather/none
 #
 #
 # COMPUTE NODES
-NodeName=$HOST State=idle Feature=dcv2,other
-# NodeName=linux[1-32] CPUs=1 State=UNKNOWN
-# NodeName=linux1 NodeAddr=128.197.115.158 CPUs=4 State=UNKNOWN
-# NodeName=linux2 NodeAddr=128.197.115.7 CPUs=4 State=UNKNOWN
-
+#
+$slurm_cfg
 PartitionName=test Nodes=$HOST Default=YES MaxTime=INFINITE State=UP
-# PartitionName=test Nodes=$HOST,linux[1-32] Default=YES MaxTime=INFINITE State=UP
 
 # DefMemPerNode=1000
 # MaxMemPerNode=1000
 # DefMemPerCPU=4000 
 # MaxMemPerCPU=4096
-
 EOF
 
 sudo cat > /etc/slurm/cgroup.conf << EOF
@@ -209,14 +317,17 @@ ConstrainRAMSpace=no
 
 EOF
 
+# >>>>>>>>>>>>>>>>
+# Set the ownerships and permissions as required.
+# >>>>>>>>>>>>>>>>
 
-sudo mkdir /var/spool/slurm
+sudo mkdir -p /var/spool/slurm
 sudo chown slurm:slurm /var/spool/slurm
 sudo chmod 755 /var/spool/slurm
-sudo mkdir /var/spool/slurm/slurmctld
+sudo mkdir -p /var/spool/slurm/slurmctld
 sudo chown slurm:slurm /var/spool/slurm/slurmctld
 sudo chmod 755 /var/spool/slurm/slurmctld
-sudo mkdir /var/spool/slurm/cluster_state
+sudo mkdir -p /var/spool/slurm/cluster_state
 sudo chown slurm:slurm /var/spool/slurm/cluster_state
 sudo touch /var/log/slurmctld.log
 sudo chown slurm:slurm /var/log/slurmctld.log
@@ -225,41 +336,55 @@ sudo chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
 
 # firewall will block connections between nodes so in case of cluster
 # with multiple nodes adapt the firewall on the compute nodes 
-#
-# sudo systemctl stop firewalld
-# sudo systemctl disable firewalld
+if [ $compute_node -eq 1 ]; then 
+    sudo systemctl stop firewalld
+    sudo systemctl disable firewalld
+fi
 
-# on the master node
-#sudo firewall-cmd --permanent --zone=public --add-port=6817/udp
-#sudo firewall-cmd --permanent --zone=public --add-port=6817/tcp
-#sudo firewall-cmd --permanent --zone=public --add-port=6818/tcp
-#sudo firewall-cmd --permanent --zone=public --add-port=6818/tcp
-#sudo firewall-cmd --permanent --zone=public --add-port=7321/tcp
-#sudo firewall-cmd --permanent --zone=public --add-port=7321/tcp
-#sudo firewall-cmd --reload
+# >>>>>>>>>>>>
+# On the head node, we need to accept the incoming connections.
+# >>>>>>>>>>>>
+if [ $head_node -eq 1 ]; then
+    sudo firewall-cmd --permanent --zone=public --add-port=6817/udp
+    sudo firewall-cmd --permanent --zone=public --add-port=6817/tcp
+    sudo firewall-cmd --permanent --zone=public --add-port=6818/tcp
+    sudo firewall-cmd --permanent --zone=public --add-port=6818/tcp
+    sudo firewall-cmd --permanent --zone=public --add-port=7321/tcp
+    sudo firewall-cmd --permanent --zone=public --add-port=7321/tcp
+fi
+
+# >>>>>>>>>>>>
+# make the changes active.
+# >>>>>>>>>>>>
+sudo firewall-cmd --reload
+
 
 # sync clock on master and every compute node 
-#sudo yum install ntp -y
-#sudo chkconfig ntpd on
-#sudo ntpdate pool.ntp.org
-#sudo systemctl start ntpd
+installit ntp
+sudo chkconfig ntpd on
+sudo ntpdate pool.ntp.org
+sudo systemctl start ntpd
 
-sudo systemctl enable slurmctld
-sudo systemctl enable slurmdbd
+if [ $head_node -eq 1 ]; then
+    sudo systemctl enable slurmctld
+    sudo systemctl enable slurmdbd
+if
 
-# on compute nodes 
-sudo systemctl enable slurmd.service
-sudo systemctl start slurmd.service
+if [ $compute_node -eq 1 ]; then 
+    sudo systemctl enable slurmd.service
+    sudo systemctl start slurmd.service
+fi
 
 echo Sleep for a few seconds for slurmd to come up ...
 sleep 3
 
-# on master
-chmod 777 /var/spool   # hack for now as otherwise slurmctld is complaining
-sudo systemctl start slurmctld.service
+if [ $head_node -eq 1 ]; then
+    chmod 777 /var/spool   # hack for now as otherwise slurmctld is complaining
+    sudo systemctl start slurmctld.service
+    echo Sleep for a few seconds for slurmctld to come up ...
+    sleep 3
+fi
 
-echo Sleep for a few seconds for slurmctld to come up ...
-sleep 3
 
 # checking 
 # sudo systemctl status slurmd.service
